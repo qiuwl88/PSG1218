8b26f14aca4381cf1d2a442025bda1eb07cb7f35
Joelyi <37099326+Joelyi@users.noreply.github.com>
2021-04-09 21:06:27 +0800

    Update and rename diy.sh to diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 
 # 去除默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改自带默认主题
-# sed -i 's/bootstrap/$default_theme/g' feeds/luci/collections/luci/Makefile
-
-# 添加 Package
-git clone https://github.com/Joelyi/luci-app-ssr-plus.git package/lean/luci-app-ssr-plus
-
-cd openwrt && ./scripts/feeds update -a
-cd openwrt && ./scripts/feeds install -a
+sed -i 's/bootstrap/$default_theme/g' feeds/luci/collections/luci/Makefile
